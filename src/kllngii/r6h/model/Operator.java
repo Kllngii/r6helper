a90a0969f21f7f3ca1df21f6aa1d6c96ce57bd37
Carsten Kelling <carsten.kelling@spiegel.de>
2017-03-13 14:16:57 +0100

    Erste Model-Klasse, noch mit Fehlern.

---
+++ src/kllngii/r6h/model/Operator.java
@@ -0,0 +1,13 @@
+package kllngii.r6h.model;
+
+import java.util.List;
+
+public class Operator {
+    
+    private final OperatorTyp typ;
+    private final String name;
+    
+    private List<String> waffen;
+    
+    
+}
