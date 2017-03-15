2404e776fc43bd91b2371152571f6868f4c53107
Lasse Kelling <lasse@kelling.de>
2017-03-15 14:40:01 +0100

    Rekruten, DLC Operator und Fuze hinzugefügt

--- src/kllngii/r6h/model/R6HelperModel.java
+++ src/kllngii/r6h/model/R6HelperModel.java
@@ -23,9 +23,11 @@ public class R6HelperModel {
 		selectedAngreifer = new ArrayList<>();
 		selectedVerteidiger = new ArrayList<>();
 		
-		//TODO Alle Operatoren mit ihre Eigenschaften definieren
+		//TODO IQ LMG hinzufügen
+		//TODO Alle Operatoren mit ihren Gadgets definieren
 		//TODO Rekrut hinzufügen
 		List<Operator> _o = new ArrayList<>();
+//		Angreifer
 		_o.add(new Operator(ANGREIFER, "Sledge", L85A2, M590A1, Arrays.asList(SMG11, P2_26)));
 		_o.add(new Operator(ANGREIFER, "Thatcher", L85A2, AR33, M590A1, Arrays.asList(P2_26)));
 		_o.add(new Operator(ANGREIFER, "Ash", G36C, R4C, Arrays.asList(_57USG, M45_MEUSOC)));
@@ -34,19 +36,32 @@ public class R6HelperModel {
 		
 		_o.add(new Operator(ANGREIFER, "Montagne", AUSF_SCHILD, Arrays.asList(P9, LFP586)));
 		_o.add(new Operator(ANGREIFER, "Glaz", OTS03, Arrays.asList(GSH18, PMM)));
-		_o.add(new Operator(ANGREIFER, "Fuze", AK12, Arrays.asList(GSH18, PMM)));
+		_o.add(new Operator(ANGREIFER, "Fuze", AK12, BAL_SCHILD, Arrays.asList(GSH18, PMM)));
 		_o.add(new Operator(ANGREIFER, "Blitz", BLSCHILD, Arrays.asList(P12)));
 		_o.add(new Operator(ANGREIFER, "IQ", AUGA2, _552C, Arrays.asList(P12)));
 		
+		_o.add(new Operator(ANGREIFER, "Buck", C8SWF, CAMRS, Arrays.asList(MK19MM)));
+		_o.add(new Operator(ANGREIFER, "Blackbeard", MK17CQB, SR25, Arrays.asList(D50)));
+		_o.add(new Operator(ANGREIFER, "Capitao", PARA308, M249, Arrays.asList(PRB92)));
+		_o.add(new Operator(ANGREIFER, "Hibana", TYPE89, SUPERNOVA, Arrays.asList(P229, BEARING9)));
+		_o.add(new Operator(ANGREIFER, "Jackal", C7E, PDW9, ITA12L, Arrays.asList(ITA12S, USP40)));
+		//TODO Alle Waffen für Rekruten
+		_o.add(new Operator(ANGREIFER, "Rekrut Blau", L85A2, M590A1, AR33, G36C, R4C, _556XI, M1014,F2, _417, SG_CQB, AK12, BAL_SCHILD, AUGA2, _552C, Arrays.asList(SMG11, P2_26)));
+		_o.add(new Operator(ANGREIFER, "Rekrut Gelb", L85A2, M590A1, AR33, G36C, R4C, _556XI, M1014,F2, _417, SG_CQB, AK12, BAL_SCHILD, AUGA2, _552C, Arrays.asList(SMG11, P2_26)));
+		_o.add(new Operator(ANGREIFER, "Rekrut Grün", L85A2, M590A1, AR33, G36C, R4C, _556XI, M1014,F2, _417, SG_CQB, AK12, BAL_SCHILD, AUGA2, _552C, Arrays.asList(SMG11, P2_26)));
+		_o.add(new Operator(ANGREIFER, "Rekrut Rot", L85A2, M590A1, AR33, G36C, R4C, _556XI, M1014,F2, _417, SG_CQB, AK12, BAL_SCHILD, AUGA2, _552C, Arrays.asList(SMG11, P2_26)));
+		_o.add(new Operator(ANGREIFER, "Rekrut Orange", L85A2, M590A1, AR33, G36C, R4C, _556XI, M1014,F2, _417, SG_CQB, AK12, BAL_SCHILD, AUGA2, _552C, Arrays.asList(SMG11, P2_26)));
 		
-		
-		
-		
+//		Verteidiger
 		_o.add(new Operator(VERTEIDIGER, "Smoke", FMG9, M590A1, Arrays.asList(SMG11, P2_26)));
-					
-
-
-//			
+		_o.add(new Operator(VERTEIDIGER, "Mute", MP5K, M590A1, Arrays.asList(P2_26)));	
+		_o.add(new Operator(VERTEIDIGER, "Castle", UMP45, M1014, Arrays.asList(M45_MEUSOC, _57USG)));
+		_o.add(new Operator(VERTEIDIGER, "Pulse", UMP45, M1014, Arrays.asList(M45_MEUSOC, _57USG)));
+		_o.add(new Operator(VERTEIDIGER, "Doc", P90, MP5, SG_CQB, Arrays.asList(LFP586, P9)));
+		
+		_o.add(new Operator(VERTEIDIGER, "Rook", P90, MP5, SG_CQB, Arrays.asList(LFP586, P9)));
+		//_o.add(new Operator(VERTEIDIGER, "Doc", P90, MP5, SG_CQB, Arrays.asList(LFP586, P9)));
+		
 //			k = "Buck";
 //			l = "Capitao";
 //			m = "Hibana";
