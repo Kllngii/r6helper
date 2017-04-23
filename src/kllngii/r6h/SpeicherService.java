52a4ce9adcdd1010a9a976cc409d603b69505544
Carsten Kelling <carsten.kelling@spiegel.de>
2017-04-23 21:08:46 +0200

    JSON gar nicht mehr loggen.

--- src/kllngii/r6h/SpeicherService.java
+++ src/kllngii/r6h/SpeicherService.java
@@ -126,7 +126,7 @@ public class SpeicherService {
         }
         
         String result = json.toString(4);
-        log.debug("JSON:\n" + result);
+//        log.debug("JSON:\n" + result);
         return result;
     }
     
