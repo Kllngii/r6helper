acd02c4bc5f7a1f7ce5f42a2160d04fcb8f4e732
Lasse Kelling <lasse@kelling.de>
2017-05-12 15:16:22 +0200

    Enum für Maps hinzu

---
+++ src/kllngii/r6h/model/Map.java
@@ -0,0 +1,44 @@
+package kllngii.r6h.model;
+
+public enum Map {
+////	Hauptspiel Maps:	////
+	Bank("/#bank/1/all", "Bank"),
+	Barlett("/#bartlett/1/all", "Barlett Universität"),
+	Chalet("/#chalet/1/all", "Chalet"),
+	Clubhaus("/#club/1/all", "Clubhaus"),
+	CafeDosto("/#kafe/1/all", "Cafe Dostjewski"),
+	Haus("/#house/1/all", "Haus"),
+	Hereford("/#hereford/1/all", "Hereford Base"),
+	Kanal("/#kanal/1/all", "Kanal"),
+	Konsulat("/#consulate/1/all", "Konsulat"),
+	Oregon("/#oregon/1/all", "Oregon"),
+	Flugzeug("/#plane/2/all", "Flugzeug"),
+	
+////	DLC Maps:	////
+//	Year 1
+	Jacht("/#yacht/2/all", "Jacht"),
+	Grenze("/#border/1/all", "Grenze"),
+	Favela("/#favela/1/all", "Favela"),
+	Wolkenkratzer("/#skyscraper/1/all", "Wolkenkratzer"),
+//	Year 2
+	Küste("/#coastline/1/all", "Küste"),
+	
+	
+	
+	;
+	private final String url;
+	private final String name;
+	
+	private Map(String url, String name){
+		this.url =url;
+		this.name =name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+}
