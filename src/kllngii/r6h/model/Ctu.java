6ca4ca7e406cf6bb2773086bce00065cb7add0bb
Lasse Kelling <lasse@kelling.de>
2017-11-24 20:19:10 +0100

    CTUs zu Waffen hinzugefügt

---
+++ src/kllngii/r6h/model/Ctu.java
@@ -0,0 +1,20 @@
+package kllngii.r6h.model;
+
+public enum Ctu {
+	SAS,
+	FBI,
+	GIGN,
+	GSG9,
+	SPETZNAS,
+	JTF2,
+	NAVYSEALS,
+	BOPE,
+	SAT,
+	GEO,
+	GROM,
+	SDU,
+	_707SBM,
+	CTU,
+	;
+	
+}
--- src/kllngii/r6h/model/Waffe.java
+++ src/kllngii/r6h/model/Waffe.java
@@ -4,94 +4,96 @@ package kllngii.r6h.model;
 public enum Waffe {
 	
 //	Sturmgewehre
-	_556XI("556 XI", Waffentyp.STURM),
-	L85A2("L85A2", Waffentyp.STURM),
-	AR33("AR33", Waffentyp.STURM),
-	G36C("G36C", Waffentyp.STURM),
-	R4C("R4C", Waffentyp.STURM),
-	F2("Famas", Waffentyp.STURM),
-	C8SWF("C8-SWF", Waffentyp.STURM),
-	AK12("Ak-12", Waffentyp.STURM),
-	AUGA2("Aug A2", Waffentyp.STURM),
-	_552C("552 Commando", Waffentyp.STURM),
-	MK17CQB("MK17-CQB", Waffentyp.STURM),
-	PARA308("Para-308", Waffentyp.STURM),
-	TYPE89("Type-89", Waffentyp.STURM),
-	C7E("C7E", Waffentyp.STURM),
-	PDW9("PDW9", Waffentyp.STURM),
+	_556XI("556 XI", Waffentyp.STURM, Ctu.FBI),
+	L85A2("L85A2", Waffentyp.STURM, Ctu.SAS),
+	AR33("AR33", Waffentyp.STURM, Ctu.SAS),
+	G36C("G36C", Waffentyp.STURM, Ctu.GSG9),
+	R4C("R4C", Waffentyp.STURM, Ctu.FBI),
+	F2("Famas", Waffentyp.STURM, Ctu.GIGN),
+	C8SWF("C8-SWF", Waffentyp.STURM, Ctu.JTF2),
+	AK12("Ak-12", Waffentyp.STURM, Ctu.SPETZNAS),
+	AUGA2("Aug A2", Waffentyp.STURM, Ctu.GSG9),
+	_552C("552 Commando", Waffentyp.STURM, Ctu.GSG9),
+	MK17CQB("MK17-CQB", Waffentyp.STURM, Ctu.NAVYSEALS),
+	PARA308("Para-308", Waffentyp.STURM, Ctu.BOPE),
+	TYPE89("Type-89", Waffentyp.STURM, Ctu.SAT),
+	C7E("C7E", Waffentyp.STURM, Ctu.GEO),
+	PDW9("PDW9", Waffentyp.STURM, Ctu.GEO),
 	
 //	Shotguns	
-	M590A1("M590A1", Waffentyp.SHOTGUN),
-	M1014("M1014", Waffentyp.SHOTGUN),
-	SG_CQB("SG CQB", Waffentyp.SHOTGUN),
-	ITA12L("Ita12 Long", Waffentyp.SHOTGUN),
-	ITA12S("Ita12 Short", Waffentyp.SHOTGUN),
-	SUPERNOVA("SuperNova", Waffentyp.SHOTGUN),
-	SASG12("SASG12", Waffentyp.SHOTGUN),
-	M870("M870", Waffentyp.SHOTGUN),
-	SUPER90("Super90", Waffentyp.SHOTGUN),
-	SPAS12("Spas-12", Waffentyp.SHOTGUN),
-	SPAS15("Spas-15", Waffentyp.SHOTGUN),
+	M590A1("M590A1", Waffentyp.SHOTGUN, Ctu.SAS),
+	M1014("M1014", Waffentyp.SHOTGUN, Ctu.FBI),
+	SG_CQB("SG CQB", Waffentyp.SHOTGUN, Ctu.GIGN),
+	ITA12L("Ita12 Long", Waffentyp.SHOTGUN, Ctu.GEO),
+	ITA12S("Ita12 Short", Waffentyp.SHOTGUN, Ctu.GEO),
+	SUPERNOVA("SuperNova", Waffentyp.SHOTGUN, Ctu.SAT),
+	SASG12("SASG12", Waffentyp.SHOTGUN, Ctu.SPETZNAS),
+	M870("M870", Waffentyp.SHOTGUN, Ctu.GSG9),
+	SUPER90("Super90", Waffentyp.SHOTGUN, Ctu.JTF2),
+	SPAS12("Spas-12", Waffentyp.SHOTGUN, Ctu.NAVYSEALS),
+	SPAS15("Spas-15", Waffentyp.SHOTGUN, Ctu.BOPE),
 	
 //	DMRs	
-	_417("417", Waffentyp.DMR),
-	OTS03("OTS-03", Waffentyp.DMR),
-	CAMRS("CamRS", Waffentyp.DMR),
-	SR25("SR-25", Waffentyp.DMR),
+	_417("417", Waffentyp.DMR, Ctu.GIGN),
+	OTS03("OTS-03", Waffentyp.DMR, Ctu.SPETZNAS),
+	CAMRS("CamRS", Waffentyp.DMR, Ctu.JTF2),
+	SR25("SR-25", Waffentyp.DMR, Ctu.NAVYSEALS),
 
 //	Pistolen u. Revolver
-	P9("P9", Waffentyp.PISTOLE),
-	LFP586("LFP 586", Waffentyp.PISTOLE),
-	PMM("PMM", Waffentyp.PISTOLE),
-	GSH18("GSH-18", Waffentyp.PISTOLE),
-	MK19MM("MK1 9mm", Waffentyp.PISTOLE),
-	PRB92("Louison", Waffentyp.PISTOLE),
-	P229("P229", Waffentyp.PISTOLE),
-	USP40("UsP40", Waffentyp.PISTOLE),
-	D50("Desert Eagle", Waffentyp.PISTOLE),
-	P12("P12", Waffentyp.PISTOLE),
-	P2_26("P226 MK 25", Waffentyp.PISTOLE),
-	_57USG("57USG", Waffentyp.PISTOLE),
-	M45_MEUSOC("M45-MEUSOC", Waffentyp.PISTOLE),
+	P9("P9", Waffentyp.PISTOLE, Ctu.GIGN),
+	LFP586("LFP 586", Waffentyp.PISTOLE, Ctu.GIGN),
+	PMM("PMM", Waffentyp.PISTOLE, Ctu.SPETZNAS),
+	GSH18("GSH-18", Waffentyp.PISTOLE, Ctu.SPETZNAS),
+	MK19MM("MK1 9mm", Waffentyp.PISTOLE, Ctu.JTF2),
+	PRB92("Louison", Waffentyp.PISTOLE, Ctu.BOPE),
+	P229("P229", Waffentyp.PISTOLE, Ctu.SAT),
+	USP40("UsP40", Waffentyp.PISTOLE, Ctu.GEO),
+	D50("Desert Eagle", Waffentyp.PISTOLE, Ctu.NAVYSEALS),
+	P12("P12", Waffentyp.PISTOLE, Ctu.GSG9),
+	P2_26("P226 MK 25", Waffentyp.PISTOLE, Ctu.SAS),
+	_57USG("57USG", Waffentyp.PISTOLE, Ctu.FBI),
+	M45_MEUSOC("M45-MEUSOC", Waffentyp.PISTOLE, Ctu.FBI),
 
 //	Reihenfeuerwaffen
-	BEARING9("Bearing 9", Waffentyp.REIHEN),
-	SMG11("SMG 11", Waffentyp.REIHEN),
+	BEARING9("Bearing 9", Waffentyp.REIHEN, Ctu.SAT),
+	SMG11("SMG 11", Waffentyp.REIHEN, Ctu.SAS),
 	
 //	Schilder
-	AUSF_SCHILD("Ausfahrbarer Schild", Waffentyp.SCHILD),
-	BLSCHILD("Blitz-Schild", Waffentyp.SCHILD),
-	BAL_SCHILD("Ballistischer Schild", Waffentyp.SCHILD),
+	AUSF_SCHILD("Ausfahrbarer Schild", Waffentyp.SCHILD, Ctu.GIGN),
+	BLSCHILD("Blitz-Schild", Waffentyp.SCHILD, Ctu.GSG9),
+	BAL_SCHILD("Ballistischer Schild", Waffentyp.SCHILD, Ctu.CTU),
 	
 //	LMGs
-	_6P41("6P41", Waffentyp.LMG),
-	M249("M249", Waffentyp.LMG),
-	G8A1("G8A1", Waffentyp.LMG),
+	_6P41("6P41", Waffentyp.LMG, Ctu.SPETZNAS),
+	M249("M249", Waffentyp.LMG, Ctu.BOPE),
+	G8A1("G8A1", Waffentyp.LMG, Ctu.GSG9),
 	
 //	MPs
-	FMG9("FMG9", Waffentyp.MP),
-	MP5K("MP5k", Waffentyp.MP),
-	UMP45("UMP45", Waffentyp.MP),
-	MP5("MP5", Waffentyp.MP),
-	P90("P90", Waffentyp.MP),
-	_9X19VSN("9X19VSN", Waffentyp.MP),
-	MP7("MP-7", Waffentyp.MP),
-	MPX("MP-X", Waffentyp.MP),
-	_416("M-416", Waffentyp.MP),
-	_9mmC1("9mm-C1", Waffentyp.MP),
-	M12("M12", Waffentyp.MP),
-	MP5SD("MP5SD", Waffentyp.MP),
-	VECTOR45("Vector.45", Waffentyp.MP)
+	FMG9("FMG9", Waffentyp.MP, Ctu.SAS),
+	MP5K("MP5k", Waffentyp.MP, Ctu.SAS),
+	UMP45("UMP45", Waffentyp.MP, Ctu.FBI),
+	MP5("MP5", Waffentyp.MP, Ctu.GIGN),
+	P90("P90", Waffentyp.MP, Ctu.GIGN),
+	_9X19VSN("9X19VSN", Waffentyp.MP, Ctu.SPETZNAS),
+	MP7("MP-7", Waffentyp.MP, Ctu.GSG9),
+	MPX("MP-X", Waffentyp.MP,Ctu.NAVYSEALS),
+	_416("M-416C", Waffentyp.MP, Ctu.GSG9),
+	_9mmC1("9mm-C1", Waffentyp.MP, Ctu.JTF2),
+	M12("M12", Waffentyp.MP, Ctu.BOPE),
+	MP5SD("MP5SD", Waffentyp.MP, Ctu.SAT),
+	VECTOR45("Vector.45", Waffentyp.MP, Ctu.GEO)
 	;
 	
 	
     private final Waffentyp typ;
 	private final String name;
+	private final Ctu c;
 	
 	
-	private Waffe(String name, Waffentyp typ) {
+	private Waffe(String name, Waffentyp typ, Ctu c) {
 		this.name = name;
 		this.typ = typ;
+		this.c = c;
 		
 	}
 
@@ -117,6 +119,13 @@ public enum Waffe {
 	            return waffe;
 	    }
 	    return null;
+	}
+
+	/**
+	 * @return c
+	 */
+	public Ctu getC() {
+		return c;
 	}
 
 }
