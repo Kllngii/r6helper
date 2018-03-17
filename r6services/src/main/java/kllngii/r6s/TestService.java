37f64fd25ce4781a52f0997ca1ada447f9c49823
Carsten Kelling <carsten.kelling@spiegel.de>
2018-03-17 15:23:04 +0100

    Beispiel-Endpoint für POST und PathParam.

--- r6services/src/main/java/kllngii/r6s/TestService.java
+++ r6services/src/main/java/kllngii/r6s/TestService.java
@@ -1,15 +1,26 @@
 package kllngii.r6s;
 
+import java.util.Date;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("test")
 public class TestService {
 
     @GET
     public String ping() {
-        return "Pong!";
+        return "Pong! " + new Date();
+    }
+    
+    @POST
+    @Path("upload/{filename}")
+    public void deletePing(@PathParam("filename") String filename, String content) {
+        //FIXME Keine gute Idee, der Aufrufer kann so beliebig viele Dateien auf unserem Server anlegen. Nur ein Beispiel für @PathParam und @POST.
+        System.out.println("Ich erzeuge jetzt die Datei " + filename + " mit Inhalt " + content);
     }
     
     @DELETE
