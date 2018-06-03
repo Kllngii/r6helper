4702e9ac84311ea97eac74c4baa57d6b25f638f5
Lasse Kelling <lasse@kelling.de>
2018-06-03 17:38:52 +0200

    Kleine Fehlerbehebungen

--- r6helper/src/kllngii/r6h/SpeicherService.java
+++ r6helper/src/kllngii/r6h/SpeicherService.java
@@ -9,13 +9,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.prefs.Preferences;
 import java.util.stream.Collectors;
 
